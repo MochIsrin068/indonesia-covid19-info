0e5307a6fc03b0988ef90dd9f15c3b8757024851
kaliwa <49640654+kaliwa@users.noreply.github.com>
2020-10-02 11:52:30 +0700

    👌 IMPROVE: error handling on detail page

--- src/services/Model.js
+++ src/services/Model.js
@@ -1,47 +1,50 @@
-import axios from "axios";
-import { API } from "./API";
+import axios from 'axios'
+import { API } from './API'
 
 const FETCH_GENERAL_DATA = async () => {
-  const response = await axios.get(`${API.BASE_URL}`);
-  return response.data;
-};
+	const response = await axios.get(`${API.BASE_URL}`)
+	return response.data
+}
 
 const FETCH_BY_CASES = async () => {
-  const response = await axios.get(API.BY_CASES);
-  return response.data;
-};
+	try {
+		return await axios.get(API.BY_CASES)
+	} catch (err) {
+		alert('Oops, tampaknya ada kesalahan saat pengambilan data')
+	}
+}
 
 const FETCH_BY_CASES_CSV = async () => {
-  const response = await axios.get(API.BY_CASES_CSV);
-  return response;
-};
+	const response = await axios.get(API.BY_CASES_CSV)
+	return response
+}
 
 const FETCH_BY_PROVINCE = async () => {
-  const response = await axios.get(API.BY_PROVINCE);
-  return response.data;
-};
+	const response = await axios.get(API.BY_PROVINCE)
+	return response.data
+}
 
 const FETCH_BY_PROVINCE_CSV = async () => {
-  const response = await axios.get(API.BY_PROVINCE_CSV);
-  return response;
-};
+	const response = await axios.get(API.BY_PROVINCE_CSV)
+	return response
+}
 
 const FETCH_BY_DAY = async () => {
-  const response = await axios.get(API.BY_DAY);
-  return response;
-};
+	const response = await axios.get(API.BY_DAY)
+	return response
+}
 
 const FETCH_BY_DAY_CSV = async () => {
-  const response = await axios.get(API.BY_DAY_CSV);
-  return response;
-};
+	const response = await axios.get(API.BY_DAY_CSV)
+	return response
+}
 
 export {
-  FETCH_GENERAL_DATA,
-  FETCH_BY_CASES,
-  FETCH_BY_CASES_CSV,
-  FETCH_BY_PROVINCE,
-  FETCH_BY_PROVINCE_CSV,
-  FETCH_BY_DAY,
-  FETCH_BY_DAY_CSV
-};
+	FETCH_GENERAL_DATA,
+	FETCH_BY_CASES,
+	FETCH_BY_CASES_CSV,
+	FETCH_BY_PROVINCE,
+	FETCH_BY_PROVINCE_CSV,
+	FETCH_BY_DAY,
+	FETCH_BY_DAY_CSV,
+}
